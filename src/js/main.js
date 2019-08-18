136ba8827898eb66fe47eb3c103661751b5994e5
Mateusz Krause <mr.mateusz.krause@gmail.com>
2019-08-18 23:37:58 +0100

    feat: storing in and getting number of glasses from local storage

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -17,17 +17,44 @@ if ('serviceWorker' in navigator) {
 // place your code below
 
 const counter = document.querySelector('.main__counter--js');
-let counterValue = 0;
 const add = document.querySelector('.main__button--add-js');
 const remove = document.querySelector('.main__button--remove-js');
 
-add.addEventListener('click', (e) => {
-    counterValue++;
-  counter.innerHTML = counterValue;
-})
-remove.addEventListener('click', (e) => {
-  if (counterValue > 0) {
-    counterValue--;
-  }
-  counter.innerHTML = counterValue;
-})
+const key = new Date().toISOString().slice(0, 10);
+
+if (localStorage.getItem(key) !== null) {
+  counter.innerHTML = localStorage.getItem(key);
+  let i;
+  add.addEventListener('click', (e) => {
+    i = localStorage.getItem(key);
+    i++;
+    localStorage.setItem(key, i);
+    counter.innerHTML = localStorage.getItem(key);
+  });
+  remove.addEventListener('click', (e) => {
+    i = localStorage.getItem(key);
+    if (i > 0) {
+      i--;
+      localStorage.setItem(key, i);
+      counter.innerHTML = localStorage.getItem(key);
+    }
+  });
+} else {
+  localStorage.setItem(key, 0);
+  counter.innerHTML = localStorage.getItem(key);
+  let i;
+  add.addEventListener('click', (e) => {
+    i = localStorage.getItem(key);
+    i++;
+    localStorage.setItem(key, i);
+    counter.innerHTML = localStorage.getItem(key);
+  });
+  remove.addEventListener('click', (e) => {
+    i = localStorage.getItem(key);
+    if (i > 0) {
+      i--;
+      localStorage.setItem(key, i);
+      counter.innerHTML = localStorage.getItem(key);
+    }
+  });
+}
